843935f6e2ad3a6b61881cadec6b443a79f89c5e
everythingforscience <yanghuidong1994@outlook.com>
2018-07-20 16:14:26 +0800

    javaEE项目初创建

---
+++ src/main/java/com/yhd/javaeeproject/JavaeeprojectApplication.java
@@ -0,0 +1,12 @@
+package com.yhd.javaeeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaeeprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaeeprojectApplication.class, args);
+    }
+}
